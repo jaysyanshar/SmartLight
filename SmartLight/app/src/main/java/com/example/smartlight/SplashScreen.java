7f635ad012a0bfe55be89cb81db2ba6bd09636b7
jaysyanshar <muhammad.jaysy.tif17@polban.ac.id>
2019-06-26 15:38:21 +0700

    Splash screen added

---
+++ SmartLight/app/src/main/java/com/example/smartlight/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.smartlight;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new CountDownTimer(4000, 1) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
+    }
+}
